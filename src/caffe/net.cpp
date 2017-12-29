aa96081b56b2e88fce43fd77776e16713ccd921e
jintian <nocholasjela@gmail.com>
2017-12-29 10:57:28 +0800

    change unreal caffe default not log prototxt message

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -51,8 +51,9 @@ void Net<Dtype>::Init(const NetParameter& in_param) {
   NetParameter filtered_param;
   FilterNet(in_param, &filtered_param);
   LOG_IF(INFO, Caffe::root_solver())
-      << "Initializing net from parameters: " << std::endl
-      << filtered_param.DebugString();
+      << "Initializing net from parameters." << std::endl
+      << "Unreal caffe will not print prototxt parameters. Check it manually.\n";
+//      << filtered_param.DebugString();
   // Create a copy of filtered_param with splits added where necessary.
   NetParameter param;
   InsertSplits(filtered_param, &param);
